ebdf894df16983fbd921d721629deeb8997862a9
Nguyen Manh Hung <hungnguyen@okxe.vn>
2020-07-09 11:54:09 +0700

    first commit

---
+++ facebook.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    document.getElementById('u_0_t').remove();
+    document.getElementById('pagelet_navigation').remove();
+    document.getElementById('fb_stories_card_root').remove();
+    document.getElementById('video_chats').remove();
+    document.getElementById('pagelet_composer').remove();
+}, 300);
+
+console.log("No More Ad - Init");
